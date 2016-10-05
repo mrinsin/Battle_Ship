1291d5449438f3f6d3d8cc28494c6e7b44dcffc8
Tim and Mrinalini <timrin@learn.com>
2016-10-05 12:14:00 -0700

    First draft of our battleship game

--- Battle_Ship.js
+++ Battle_Ship.js
@@ -1,17 +1,18 @@
-
+var html = "";
 //Start Game
 console.log("The Game starts");
 
 $(document).ready(function() {
 
-for(counter = 0; counter < 10; counter++) {
-  $("table").append("<tr>");
+  for(counter = 0; counter < 10; counter++) {
+    html = html + "<tr>";
 
-  for(counterTwo = 0; counterTwo < 10; counterTwo++) {
-  $("table").append("<td id=" + counter + counterTwo + ">mrim</td>");
+    for(counterTwo = 0; counterTwo < 10; counterTwo++) {
+      html = html + "<td id=" + counter + counterTwo + ">mrim</td>";
     }
+
+    html = html + "</tr>"
   }
 
-  $("table").append("</tr>");
-
+$("table").append(html);
 });
